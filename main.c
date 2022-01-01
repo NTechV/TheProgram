281af3d0a2d49192c2a6f3385ea6060ce8bed267
NTechV <96956418+NTechV@users.noreply.github.com>
2022-01-01 13:34:19 -0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define CON 72
+
+
+
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	printf("%i",CON);
+
+	return 0;
+}
